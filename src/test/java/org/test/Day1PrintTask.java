f8d154a1171455230928a7359751fb78b56287ff
Hemamalini-31 <vhemamalini31@gmail.com>
2022-04-20 22:26:42 +0530

    Hema's code

--- src/test/java/org/test/Day1PrintTask.java
+++ src/test/java/org/test/Day1PrintTask.java
@@ -3,5 +3,6 @@ package org.test;
 public class Day1PrintTask {
 public static void main(String[] args) {
 	System.out.println("task of printing price of iphones");
+	System.out.println("task of ascending prices");
 }
 }
